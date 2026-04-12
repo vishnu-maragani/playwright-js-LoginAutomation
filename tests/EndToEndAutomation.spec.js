d1970b9533464e2fd0297d0dba61af2cc11d1d75
Vishnu Maragani <vishnu.maragani@in.techwave.net>
2026-04-12 18:33:45 +0530

    final code

--- tests/EndToEndAutomation.spec.js
+++ tests/EndToEndAutomation.spec.js
@@ -16,64 +16,28 @@ test.only("E2E Test case Scenario", async ({ browser }) => {
   await page.locator("#userPassword").fill(password);
   await page.locator("[name='login']").click();
 
-  //Grabbing product Items list
-  const buyItems = ["zara", "iphone"];
-  const itemLocator = page.locator(".card-body");
-  await itemLocator.last().waitFor();
-
-  for (const product of buyItems) {
-    const matchCard = itemLocator.filter({ hasText: new RegExp(product, 'i')});
-    if ((await matchCard.count()) > 0) {
-      console.log(`Clicking add to cart for: ${product}`); // ← debug
-      await matchCard.first().locator(".w-10").click({force:true});
-      await page.waitForTimeout(2000);
-    }
-  };
-
-
-  //Cliicking on cart button and validating added products
+  //Adding Items list
+  const buyItem = 'zara';
+  await page.locator('.card-body').filter({hasText:buyItem}).getByRole('button',{name:'Add To Cart'}).click();
   await page.locator("[routerlink*='cart']").click();
-  const productsAddedCart = page.locator(".cart");
-  await productsAddedCart.last().waitFor();
-  for (const item of buyItems) {
-    await expect(productsAddedCart).toContainText(item, {ignoreCase: true});
-  };
+ 
+  //Vrryfing added item in cart section
+  await expect(page.locator('.cart').filter({hasText:buyItem})).toBeVisible();
 
-  //Checkout button clicking
-  await page.locator(".totalRow button").click(); 
-  await page.locator("[placeholder*='Country']").pressSequentially("indi");
-  const dropDown = page.locator(".ta-results");
-  await dropDown.waitFor();
-  const optionCount = await dropDown.locator("button").count();
-  for (let i = 0; i < optionCount; i++) {
-    const text = await dropDown.locator("button").nth(i).textContent();
-    if (text === " India") {
-      await dropDown.locator("button").nth(i).click();
-      break;
-    }
-  };
+  //Checkout  & Place Order
+  await page.locator("[style*='none'] button").click();
+  await expect(page.getByPlaceholder('Select Country')).toBeVisible();
+  await page.getByRole('textbox',{name:'Select Country'}).pressSequentially('indi');
+  await page.locator('.ta-results button').filter({hasText:' India'}).nth(1).click();
+  await page.locator('.btnn').click();
+  
 
-  await expect(page.locator(".user__name label")).toHaveText(username);
-  await page.locator(".actions a").click(); //Placing order
-  await expect(page.locator(".hero-primary")).toContainText(
-    "Thankyou for the order",
-  );
-
-  const DumpIds = await page
-    .locator(".em-spacer-1 .ng-star-inserted")
-    .allTextContents();
-  const orderIds = DumpIds.map((ele) => ele.replace(/\|/g, "").trim());
-  console.log(orderIds);
-  await page.locator("li [routerlink*='myorders']").click(); //Going to orders section
-  const ordersLocators = page.locator("tbody tr");
-  for (let i = 0; i < (await ordersLocators.count()); i++) {
-    const id = await ordersLocators.nth(i).locator("th").textContent();
-    console.log(id);
-    if (orderIds.map((ele) => ele.trim()).includes(id)) {
-      await ordersLocators.nth(i).locator("button").click();
-      break;
-    }
-  }
-
-  // await page.pause();
+  //Product order confirmation
+  await expect(page.locator('.hero-primary')).toContainText(' Thankyou for the order. ');
+  const RaworderID = await page.locator('label.ng-star-inserted').textContent();
+  const orderId = RaworderID.replace(/\|/g, "")
+  await page.getByText('Orders History Page').click();
+  await page.locator('.table').waitFor();
+  const allId =await page.locator('tbody tr th').allTextContents();
+  expect(allId.some(ele=>orderId.includes(ele))).toBe(true);
 });
