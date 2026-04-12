b077e9f44b32eb1dd5de53f85bed4efb7052729d
Vishnu Maragani <vishnu.maragani@in.techwave.net>
2026-04-12 14:09:20 +0530

    E2E test Other way change

--- tests/EndToEndAutomation_OtherWay.spec.js
+++ tests/EndToEndAutomation_OtherWay.spec.js
@@ -13,12 +13,13 @@
         //Login to the Application
         await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
 
-        await page.getByPlaceholder('email@example.com').fill('anonymous@gmail.com');
+        // await page.getByPlaceholder('email@example.com').fill('anonymous@gmail.com');
+        await page.getByPlaceholder('email@example.com').fill(username);
         await page.getByPlaceholder('enter your passsword').fill(password);
         await page.getByRole('button',{name:'Login'}).click();
 
-        await page.getByPlaceholder('email@example.com').fill(username);
-        await page.getByRole('button',{name:'Login'}).click();
+        // await page.getByPlaceholder('email@example.com').fill(username);
+        // await page.getByRole('button',{name:'Login'}).click();
         await page.waitForURL('**/dashboard/dash');  
         await expect(page.locator('.card-body').first()).toBeVisible();
         await page.locator('.card-body').filter({hasText:'ADIDAS ORIGINAL'}).getByRole('button',{name:'Add To Cart'}).click();
@@ -29,5 +30,4 @@
         await page.getByRole('button',{name:'India'}).nth(1).click();
         await page.getByText('Place Order').click();
         await page.getByText(' Thankyou for the order. ').isVisible();
-        await page.pause();
     });
